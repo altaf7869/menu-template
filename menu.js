d8e9a78e0b189548c3aff185d0b83d5a87d0ff6a
altaf7869 <Altaf@7869>
2025-07-29 20:34:45 +0530

    changes commit

---
+++ menu.js
@@ -0,0 +1,51 @@
+const menuData = [
+  {
+    "name": "Afghani Tandoori",
+    "category": "starter",
+    "image": "assets/images/Tandoori-Starter/afghani.jpg",
+    "half": 120,
+    "full": 200
+  },
+  {
+    "name": "Paneer Chilly",
+    "category": "chinese",
+    "image": "assets/images/Chinese/paneerchilly.jpg",
+    "half": 110,
+    "full": 180
+  },
+  {
+    "name": "Mutton Rogan Josh",
+    "category": "mutton",
+    "image": "assets/images/Mutton/rogan.jpg",
+    "half": 180,
+    "full": 300
+  },
+  {
+    "name": "Tandoori Roti",
+    "category": "roti",
+    "image": "assets/images/Roti/roti.jpg",
+    "half": null,
+    "full": 20
+  },
+  {
+    "name": "Fish Fry",
+    "category": "fish",
+    "image": "assets/images/Fish/fishfry.jpg",
+    "half": 150,
+    "full": 250
+  },
+  {
+    "name": "Masala Papad",
+    "category": "papad",
+    "image": "assets/images/Papad/masalapapad.jpg",
+    "half": null,
+    "full": 30
+  },
+  {
+    "name": "Veg Pulao",
+    "category": "rice",
+    "image": "assets/images/Rice/vegpulao.jpg",
+    "half": 80,
+    "full": 140
+  }
+];
